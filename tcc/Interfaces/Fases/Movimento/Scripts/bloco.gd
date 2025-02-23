e26bc505388558e3331dd0cad6d1e3b97923b808
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2025-02-22 23:55:50 -0400

    Adicionado arrastar

--- tcc/Interfaces/Fases/Movimento/Scripts/bloco.gd
+++ tcc/Interfaces/Fases/Movimento/Scripts/bloco.gd
@@ -1,4 +1,24 @@
 extends TextureButton
 
+var posicaoInicial: Vector2 
+var arrastando = false
+var deslocamento: Vector2
+
 func _ready() -> void:
-	pass
+	posicaoInicial = global_position
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		arrastando = true
+		deslocamento = get_global_mouse_position() - global_position
+	elif event is InputEventMouseButton and not event.pressed:
+		arrastando = false		
+
+func _process(delta: float) -> void:
+	if arrastando:
+		global_position = get_global_mouse_position() - deslocamento
+	else:
+		global_position = get_meta("posicao_inicial", global_position)
+
+
+		
